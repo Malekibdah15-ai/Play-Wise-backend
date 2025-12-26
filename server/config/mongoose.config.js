1ee8b50e4742fd4383c9d7c4032643e282c78c14
Anas hawwash <hawwashanas@gmail.com>
2025-12-26 13:48:22 +0200

    Login And Reg

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,12 +1,29 @@
 const mongoose  = require('mongoose')
 require('dotenv').config()
-const dbName = process.env.DB;
+const dbName = process.env.Db;
 const username = process.env.ATLAS_USERNAME;
 const pw = process.env.ATLAS_PASSWORD;
+console.log(dbName);
+console.log(username);
+console.log(dbName);
 
 
-const uri = `mongodb+srv://${username}:${pw}@cluster0.oo5frow.mongodb.net/${dbName}?appName=Cluster0`;
 
-mongoose.connect(uri)
-    .then(() => console.log("Established a connection to the database"))
-    .catch(err => console.log("Something went wrong when connecting to the database", err));
+// this is malek route for the db 
+// const uri = `mongodb+srv://${username}:${pw}@cluster0.oo5frow.mongodb.net/${dbName}?appName=Cluster0`;
+
+
+
+// and this is anas route for conneiction
+const uri = `mongodb+srv://${username}:${pw}@testc.hrnc0an.mongodb.net/${dbName}?appName=TestC`
+
+async function connectDB() {
+  try {
+    await mongoose.connect(uri);
+    console.log("Established a connection to the database");
+  } catch (err) {
+    console.log("Something went wrong when connecting to the database", err);
+  }
+}
+
+connectDB();
