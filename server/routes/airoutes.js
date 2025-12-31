2000de9113aec514604c15b0954f836907f150d0
Malekibdah15-ai <mibdah15@gmail,com>
2025-12-31 09:04:29 +0200

    airoutes

--- server/routes/airoutes.js
+++ server/routes/airoutes.js
@@ -1,9 +1,21 @@
 const express = require("express");
-const { getRecommendations ,getNews } = require("../controllers/aicontroller");
+const router = express.Router();
 
+const { getRecommendations, getNews } = require("../controllers/aicontroller");
+const { getDailyChallenges } = require("../controllers/challengesController");
+const { filterMessage } = require("../controllers/chatFilterController");
+const { getBestDeal } = require("../controllers/dealsController");
+const { matchmaker } = require("../controllers/matchmakerController");
+
+// GET routes
+router.get("/challenges", getDailyChallenges);
+
+// POST routes
+router.post("/chat-filter", filterMessage);
+router.post("/deal", getBestDeal);
+router.post("/matchmaker", matchmaker);
 router.post("/recommend", getRecommendations);
 router.post("/news", getNews);
+
 module.exports = router;
-
-
 
